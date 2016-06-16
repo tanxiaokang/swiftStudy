775e7cdfe6c03ac41574de03740dfe771668cf99
檀小康 <tanxiaokang@99fenqi.com>
2016-06-16 10:02:12 +0800

    ..switch

--- println/ViewController.swift
+++ println/ViewController.swift
@@ -13,124 +13,46 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var myVariable = 42
-        
-        print(myVariable)
-        
-        myVariable = 50
-        
-        print(myVariable)
-        
-        let myConstant = 42
-        print(myConstant)
-        
-        //1.如果初始值没有提供足够的信息 需要在后面声明类型,用冒号分隔
-        
-        let implicitInteger = 70
-        print(implicitInteger)
-        let implicitDouble = 70.0
-        print(implicitDouble)
-//        let implocitDouble: Double = 70
-//        print(implicitDouble)
-        
-        
-        
-        print("hello, word")
-     
-        
-        //1.1 创建一个常量,显式指定类型为Float 并指定初始值为4
-        
-        let label = "The width is "
-        
-        let width = 94
-        
-        let widthLabel = label + String(width)
-        
-        print(widthLabel)
-        
-        //1.1.1 更简单 把值转换成字符串的方式:把值写到括号中,并且在括号之前写一个反斜杠
-        
-        let apples:Double = 3
-        
-        let oranges:Double = 5
-        
-        let name = "bob"
-        
-        let appleSummary = name + " I have \(apples) apples."
-        
-        print(appleSummary)
-        
-        let fruitSummary = "I have \(apples + oranges) pieces of fruit. "
+        //1.switch 支持任意类型的数据以及各种比较操作 --- 不仅仅是证书以及测试相等
 
-        print(fruitSummary)
+        let vegetable = "red pepper pepper"
+        
+        switch vegetable {
+        case "red pepper":
+            let vegetableComment = "Add some raisins and make ants on a log."
+            print(vegetableComment)
+        case "c ucumb er", "wat ercre ss":
+            let vegetableComment = "That would make a good tea sandwich."
+            print(vegetableComment)
+        case let x where x.hasPrefix("red"):
+            let vegetableComment = "Is it a spicy \(x)?"
+            print(vegetableComment)
+            
+        default:
+            let vegetableComment = "Everything tastes good in soup."
+            print(vegetableComment)
+        }
         
         
-        
-        
-        //2.0 使用方括号[]来创建数组和字典,并使用下标或者键(key)来访问元素
-        
-        var shoppingList = ["catfish","water","tulips","blue paint"]
-        
-        print(shoppingList[1])
-        
-        
-        var occupations = [
-            "Malcolm":"Captain",
-            "Keylee":"Mechanic",
+        //2.可以使用forin 来遍历字典, 需要两个变量来表示每个键值对
+        let interestingNumbers = [
+            "Prime":    [2, 3, 5, 7, 11, 13],
+            "Fibonacci":[1, 1, 2, 3, 5, 8],
+            "Square":   [1, 4, 9, 16, 25],
         ]
-        occupations["Jayne"] = "Public Relations";
+        var largest = 0
         
-        print(occupations["Keylee"])
-        print(occupations)
+        for (kind, numbers) in interestingNumbers {
+            print(kind)
+            print(numbers)
         
-        //2.1 初始化语法 创建一个空数组或者字典
-        let emotyArray = []
-        
-        print(emotyArray)
-        
-        let emptyDictionary = Dictionary<NSString,Float>()
-        
-        print(emptyDictionary)
-        
-        //3.0 控制流
-        
-        //使用if 和 switch 来进行条件操作, 使用for-in for while do-while 来进行循环.包裹条件和循环变量括号可以省略 但是语句体的大括号是必须的
-        
-        let indicidiualScores = [11, 22,33,44,55,66,77]
-        
-        var teamScore = 0
-        
-        for score in indicidiualScores {
-            if score > 30 {
-                teamScore += 3
-                print(teamScore)
-            }else{
-                teamScore += 1
-                print(teamScore)
+            for number in numbers {
+                if number > largest {
+                    largest = number
+                }
             }
         }
-        print(teamScore)
-        
-        
-        
-        //有些变量的值是可选的.一个可选的值可能是一个具体的值或者nil 表示值缺失.在类型后面加一个问号来标记这个变量的值是可选的
-//        
-//        var optionalString:String? = "Hello"
-//        
-//        optionalString = nil
-        
-        var optionalName: String? = "John Appleseed"
-        optionalName = nil
-        var greeting = "Hello!"
-        if let names = optionalName {
-            greeting = "Hello,\(names)"
-             print(greeting)
-        }else{
-            greeting = "Hello,qwe"
-            print(greeting)
-        }
-       
-        
+        print(largest)
         
         
     }
